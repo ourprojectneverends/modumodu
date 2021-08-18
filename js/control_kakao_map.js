eef692f89e99e69150beb05cf46b73d5ece25ae6
YAMCHA <dbals9812@naver.com>
2021-08-18 16:39:50 +0900

    add marker click event

--- js/control_kakao_map.js
+++ js/control_kakao_map.js
@@ -43,5 +43,24 @@ $.getScript(`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&autoload=fa
 
       var map = new kakao.maps.Map(container, options);
       marker.setMap(map);
+
+      // 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    
+  // 클릭한 위도, 경도 정보를 가져옵니다 
+  var latlng = mouseEvent.latLng; 
+  
+  // 마커 위치를 클릭한 위치로 옮깁니다
+  marker.setPosition(latlng);
+  
+  var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+  message += '경도는 ' + latlng.getLng() + ' 입니다';
+  
+  var resultDiv = document.getElementById('clickLatlng'); 
+  resultDiv.innerHTML = message;
+  
+});
     });
 })
+
