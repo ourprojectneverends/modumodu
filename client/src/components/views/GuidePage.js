1d032ef0f7384937212af9de37ed69fda44df12f
YAMCHA <dbals9812@naver.com>
2021-09-08 23:50:15 +0900

    [EDIT] warning 해결

--- client/src/components/views/GuidePage.js
+++ client/src/components/views/GuidePage.js
@@ -13,7 +13,7 @@ function GuidePage() {
                         모두모두 홈에서 [모두모두로 간편하게 약속잡기] 버튼 또는 상단
                         네비게이션 바에서 [START]-[새 모임 만들기] 로 접속합니다.
                     </p>
-                    <img src="./img/map.png"></img>
+                    <img src="./img/map.png" alt=""></img>
                 </div>
                 <div className="guide-step">
                     <p>2. 정보 입력</p>
@@ -22,7 +22,7 @@ function GuidePage() {
                         입력합니다. 잘못 입력한 부분이 있다면 이전 버튼을 통해 수정할 수
                         있습니다.
                     </p>
-                    <img src="./img/map.png"></img>
+                    <img src="./img/map.png" alt=""></img>
                 </div>
                 <div className="guide-step">
                     <p>3. 초대하기</p>
@@ -32,7 +32,7 @@ function GuidePage() {
                         카카오톡 공유하기를 통해 이전 단계에서 정한 최대 인원까지 다른
                         사람을 모임에 초대할 수 있습니다.
                     </p>
-                    <img src="./img/map.png"></img>
+                    <img src="./img/map.png" alt=""></img>
                 </div>
                 <div className="guide-step">
                     <p>4. 모이기</p>
@@ -40,7 +40,7 @@ function GuidePage() {
                         다른 참여자가 생길 경우 결과 위치가 참여자에 따라 실시간으로
                         변경됩니다. 자! 이제 정해진 장소로 모임을 시작해 볼까요?
                     </p>
-                    <img src="./img/map.png"></img>
+                    <img src="./img/map.png" alt=""></img>
                 </div>
             </div>
         </div>
